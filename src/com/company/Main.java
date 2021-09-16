4c1f099dad6458f8a4b88b101671eff3dd7eac4e
irenejoeunpark <irenepark1221@gmail.com>
2021-09-16 13:19:27 -0400

    Argument option added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,94 @@
 package com.company;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String txtFileName = fileNameReader(args);
+        System.out.println(args[0]);
+
+        if(args[0].equals("--version")|| args[0].equals("-v")){
+            System.out.println("ssgApplication ver 1.0.0");
+        } else if(args[0].equals("--help") || args[0].equals("-h")){
+            System.out.println("---------------------------------------");
+            System.out.println("* -v OR --version - current version   *");
+            System.out.println("* -i OR --input - input file or files *");
+            System.out.println("---------------------------------------");
+
+        } else if(args[0].equals("--input") || args[0].equals("-i")){
+            String[] fileNameFull = new String[args.length-1];
+            for(int i=1;i<args.length;i++){
+                fileNameFull[i-1] = args[i];
+            }
+
+            String inputFile = fileNameReader(fileNameFull);
+            if(inputFile.endsWith(".txt")){
+                //if input file is only one file
+                File file = new File(inputFile);
+                String fileName = file.getName();
+                //create html
+                createHtml(file);
+
+            }else {
+                //if user added multiple files(directory)
+                try {
+                    List<File> allFiles = Files.walk(Paths.get(inputFile))
+                            .filter(Files::isRegularFile)
+                            .map(Path::toFile)
+                            .collect(Collectors.toList());
+                    List<String> fileNames = new ArrayList<>() ;
+                    for(File file :allFiles){
+//                        fileNames.add(file.getName());
+                        createHtml(file);
+                    }
+//
+//                    for(String filename : fileNames){
+//                        //create html
+//
+//                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
 
 
-        String srcFile = "sources\\Sherlock-Holmes-Selected-Stories\\" + txtFileName;
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(srcFile),"UTF-8"));
+
+        }else {
+            System.out.println("Invalid argument passed. Please pass a valid argument");
+        }
+
+
+    }
+
+
+    private static String fileNameReader(String[] str) {
+        StringBuilder s = new StringBuilder();
+        for(String arg: str) {
+            s.append(arg).append(" ");
+        }
+        return s.substring(0,s.length() -1);
+    }
+
+    private static void createHtml(File fileName){
+        try{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName),"UTF-8"));
             String aLine;
 
-            String fileName = txtFileName.split("\\.")[0];
-            String htmlFileName = "dist/" + fileName + ".html";
+
+            String fName = fileName.toString().split("\\.")[0];
+            String[] getOnlyName = fName.split("\\\\");
+            String name = getOnlyName[getOnlyName.length-1];
+
+            String htmlFileName = "dist/" + name + ".html";
+
 
             String beginParagraph = "<p>";
             String endPragraph = "</p>";
@@ -25,7 +98,7 @@ public class Main {
                     + "<head>\r\n"
                     + "  <meta charset=\"utf-8\">\r\n"
                     + "  <title>"
-                    + fileName
+                    + name
                     + "</title>\r\n"
                     + "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\r\n"
                     + "</head>\r\n"
@@ -35,12 +108,9 @@ public class Main {
             String htmlFormE = "</body>\r\n"
                     + "</html>\r\n"
                     + "";
-
             new File("dist").mkdir();
 
-
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(htmlFileName),"UTF-8"));
-
             writer.write(htmlFormB);
             aLine = reader.readLine();
             writer.write("<h1>"+aLine + "</h1>");
@@ -60,17 +130,12 @@ public class Main {
             writer.close();
 
 
+            System.out.println(name + ".html has been created in dist!");
+
         }catch(IOException e) {
             e.printStackTrace();
         }
     }
 
 
-    private static String fileNameReader(String[] str) {
-        StringBuilder s = new StringBuilder();
-        for(String arg: str) {
-            s.append(arg).append(" ");
-        }
-        return s.substring(0,s.length() -1);
-    }
 }
