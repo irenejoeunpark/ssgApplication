c5090e90767374279ce08a4dd9439aa90e7cbf0e
Diana Belokon <belokondiana.786@gmail.com>
2021-09-23 19:42:08 -0400

    Parse double asterisks as <strong>

---
+++ src/com/company/MarkdownParser.java
@@ -0,0 +1,82 @@
+package com.company;
+
+public class MarkdownParser {
+    private StringBuilder htmlBuffer = new StringBuilder();
+    private boolean isFirstPairOfAsterisksFound = false;
+    private boolean isFirstAsteriskFound = false;
+    int indexForOpeningDelimiter = -1;
+    int indexOfLastAppearingWhitespace = -1;
+
+    public void acceptCharacter(char character) {
+        if (character == '*') {
+            if (!haveFoundFirstAsterisk()) {
+                isFirstAsteriskFound = true;
+                push(character);
+            } else if (!haveFoundFirstAsteriskPair()) {
+                isFirstAsteriskFound = false;
+                isFirstPairOfAsterisksFound = true;
+                indexForOpeningDelimiter = getCurrentCharacterIndex() - 1;
+                push(character);
+            } else if (!isSecondPairPrecededBySpace()) {
+                replaceAtBufferWith(indexForOpeningDelimiter, 2, "<strong>");
+                replaceAtBufferWith(getCurrentCharacterIndex() - 1, 1, "</strong>");
+                isFirstAsteriskFound = false;
+                isFirstPairOfAsterisksFound = false;
+            }
+        } else {
+            if (character == ' ') {
+                indexOfLastAppearingWhitespace = getCurrentCharacterIndex();
+
+                if (isFirstAsteriskPairFollowedBySpace()) {
+                    isFirstPairOfAsterisksFound = false;
+                    indexForOpeningDelimiter = -1;
+                }
+            }
+            isFirstAsteriskFound = false;
+            push(character);
+        }
+    }
+
+    private void replaceAtBufferWith(int start, int length, String s) {
+        htmlBuffer.delete(start, start + length);
+        htmlBuffer.insert(start, s);
+    }
+
+    private boolean isSecondPairPrecededBySpace() {
+        return getCurrentCharacterIndex() - indexOfLastAppearingWhitespace == 2;
+    }
+
+    private boolean haveFoundFirstAsteriskPair() {
+        return isFirstPairOfAsterisksFound;
+    }
+
+    public String returnParsedLine() {
+        return htmlBuffer.toString();
+    }
+
+    private boolean haveFoundFirstAsterisk() {
+        return isFirstAsteriskFound;
+    }
+
+    private int getCurrentCharacterIndex() {
+        return htmlBuffer.length();
+    }
+
+    private void push(char character) {
+        htmlBuffer.append(character);
+    }
+
+    private boolean isFirstAsteriskPairFollowedBySpace() {
+        return indexForOpeningDelimiter >= 0 && indexOfLastAppearingWhitespace - indexForOpeningDelimiter == 2;
+    }
+
+    public static String parseMarkdownLine(String rawText) {
+        MarkdownParser parser = new MarkdownParser();
+
+        for (int i = 0; i < rawText.length(); ++i) {
+            parser.acceptCharacter(rawText.charAt(i));
+        }
+
+        return parser.returnParsedLine();
+    }
+}
