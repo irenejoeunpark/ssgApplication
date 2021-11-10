be449b1d8e4f4547c9906e88900b38d3b832246c
Irene Park <irenepark1221@gmail.com>
2021-11-10 17:01:40 -0500

    Added Junit dependencies

---
+++ src/test/java/HTMLBuilderTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HTMLBuilderTest {
+
+    @Test
+    void processInput() {
+    }
+
+    @Test
+    void fileNameReader() {
+    }
+
+    @Test
+    void writeHtmlHeader() {
+    }
+
+    @Test
+    void writeHtmlFoot() {
+    }
+}
